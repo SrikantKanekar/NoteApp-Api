45833bcaaf23717c07ad9a4b249f676e2b144c76
SrikantKanekar <kanekarsrikant01@gmail.com>
2021-04-12 17:53:38 +0530

    added embedded server

--- src/Application.kt
+++ src/Application.kt
@@ -7,37 +7,39 @@ import io.ktor.auth.*
 import io.ktor.features.*
 import io.ktor.routing.*
 import io.ktor.serialization.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+fun main(args: Array<String>) {
+    val port = System.getenv("PORT")?.toInt() ?: 23567
+    embeddedServer(Netty, port){
 
-@Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
+        install(ContentNegotiation) {
+            json()
+        }
 
-    install(ContentNegotiation) {
-        json()
-    }
+        install(Authentication) {
+            basic {
+                realm = "Note Server"
+                validate { credentials ->
+                    val email = credentials.name
+                    val password = credentials.password
 
-    install(Authentication) {
-        basic {
-            realm = "Note Server"
-            validate { credentials ->
-                val email = credentials.name
-                val password = credentials.password
-
-                when (checkPasswordForEmail(email, password)) {
-                    true -> UserIdPrincipal(email)
-                    false -> null
+                    when (checkPasswordForEmail(email, password)) {
+                        true -> UserIdPrincipal(email)
+                        false -> null
+                    }
                 }
             }
         }
-    }
 
-    routing {
-        testRoute()
-        registerRoute()
-        loginRoute()
-        noteRoute()
-        deletedNoteRoute()
-    }
+        routing {
+            testRoute()
+            registerRoute()
+            loginRoute()
+            noteRoute()
+            deletedNoteRoute()
+        }
+    }.start(wait = true)
 }
+
