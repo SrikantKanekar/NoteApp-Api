aee2a04fee90ac9eaee9d59464209791dc3af6cb
srikant <kanekarsrikant01@gmail.com>
2023-01-14 19:28:02 +0530

    labels equals an hashcode

--- src/main/kotlin/com/example/model/Label.kt
+++ src/main/kotlin/com/example/model/Label.kt
@@ -8,4 +8,19 @@ data class Label(
     val name: String,
     val createdAt: String,
     val updatedAt: String
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Label
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
